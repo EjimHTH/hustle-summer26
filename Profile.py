a77b93ec933c2246a22499fa0d37ddcbf62e4b97
Edgar Jimenez <edgarjimenez@Edgars-MacBook-Pro.local>
2026-06-17 16:47:53 -0700

    Add Python profile variables lab

---
+++ Profile.py
@@ -0,0 +1,17 @@
+full_name = "Edgar Jimenez"
+
+nickname = "Edgar"
+
+age = 25
+
+used_python_before = False
+
+hobbies = ["Reading", "Designing","Working out", "Traveling"]
+
+favorites = {
+    "movie" : "The Incredibles",
+    "food": "Tacos",
+    "hobby" : "Graphic Design"
+}
+
+print (full_name, age, hobbies)
